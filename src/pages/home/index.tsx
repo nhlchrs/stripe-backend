65a0e123e0b839fff2ce77ec18b1e42d1d5aa64a
Ivtidai Warsi <ivtidai@hotdesk.io>
2022-09-23 10:33:33 +0530

    Change home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,8 @@
 import Layout from '../../layout';
 import { Link } from 'react-router-dom'
+import Product from '../../components/product';
+import { ProductType } from '../../types/product.type';
+import { clientProducts } from '../../dummyData/product';
 import { clients } from "../../dummyData/client";
 
 const Home = () => {
@@ -11,8 +14,21 @@ const Home = () => {
                         <Link to={`/${client.id}`}>{client.name}</Link>
                     </div>
                 )))}
-
             </div>
+            <section id="services" className="services">
+                <div className="container" data-aos="fade-up">
+                    <div className="section-title">
+                        <h3>New <span>Products</span></h3>
+                    </div>
+                </div>
+                <div className="row">
+                    {Object.values(clientProducts).map((products: ProductType[]) =>
+                        products.map((product: ProductType, index) => (
+                            <Product {...product} key={`product-${index}`}></Product>
+                        ))
+                    )}
+                </div>
+            </section>
         </Layout>
     )
 }
